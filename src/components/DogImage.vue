c84bf95e4c2cba26f5dc9720d9d8dfc704a34206
Rocio Torres <rocio.tp@uc.cl>
2023-06-04 13:01:56 -0400

    Solo un perro

---
+++ src/components/DogImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <img class="dog-image" :src=data>
+</template>
+ 
+<script>
+    export default {
+    props: ['data']
+    }
+</script>
+
+<style scoped>
+    .dog-image{
+        width: 18rem;
+        display: block;
+        padding: 3px 3px;
+    }
+</style>
--- src/components/Dogs.vue
+++ src/components/Dogs.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Dog from './Dog.vue';
+    import DogImage from './DogImage.vue';
 </script>
 
 <template>
     <div class="dogs-container">
         <div class="dog-container" v-for="item in listItems">
-            <Dog :data="item.attributes"></Dog>
+          <DogImage :data="pic"></DogImage>
+          <Dog :data="item.attributes"></Dog>
         </div>
     </div>
 </template>
@@ -14,11 +16,13 @@
 <script>
     export default {
     components: {
+        DogImage,
         Dog
     },
     data() {
       return {
-        listItems: [],
+        pic: String,
+        listItems: []
       }
     },
     methods: {
@@ -26,6 +30,10 @@
         const res = await fetch("https://dogapi.dog/api/v2/breeds");
         const finalRes = await res.json();
         this.listItems = finalRes.data;
+        const res_photos = await fetch("https://dog.ceo/api/breed/ovcharka/images/random/1");
+        const finalRes_photos = await res_photos.json();
+        this.listPhotos = finalRes_photos["message"];
+        this.pic = this.listPhotos;
       }
     },
     mounted() {
@@ -39,18 +47,20 @@
     .dogs-container{
         flex-direction: row;
         flex-wrap: wrap;
+        justify-content: center;
     }
 
     .dog-container {
         /* Add shadows to create the "card" effect */
         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
         transition: 0.3s;
-    	width: 20rem;
+    	  width: 20rem;
         border-color: rgb(30 64 175);
         border-width: 4px;
         flex-direction: column;
         margin: 1.5rem; /* 20px */
         border-radius: 0.5rem;
+        justify-self: center;
     }
 
     /* On mouse-over, add a deeper shadow */
