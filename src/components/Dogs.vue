bbeee02ef29554d42187ca2b8bab072dda20dde0
alfre <alfredo.medina@uc.cl>
2023-06-05 15:02:58 -0400

    feat: pagination

--- src/components/Dogs.vue
+++ src/components/Dogs.vue
@@ -4,6 +4,11 @@ import DogImage from "./DogImage.vue";
 </script>
 
 <template>
+  <div class="pagination">
+    <button v-if="currentPage > 1" @click="prevPage">Prev</button>
+    <span class="pages-span">Page {{ currentPage }} of {{ totalPages }}</span>
+    <button v-if="currentPage < totalPages" @click="nextPage">Next</button>
+  </div>
   <div class="dogs-container">
     <div class="dog-container" v-for="item in listItems">
       <DogImage :name="item.attributes"></DogImage>
@@ -22,20 +27,36 @@ export default {
     return {
       pic: String,
       listItems: [],
+      currentPage: 1,
+      totalPages: 0,
     };
   },
   methods: {
-    async getData() {
-      const res = await fetch("https://dogapi.dog/api/v2/breeds");
-      // const res = await fetch("https://dog.ceo/api/breeds/list/all");
+    async getData(page = 1) {
+      const res = await fetch(
+        `https://dogapi.dog/api/v2/breeds?page[number]=${page}`
+      );
       const finalRes = await res.json();
       this.listItems = finalRes.data;
+      this.totalPages = 29;
       // const res_photos = await fetch(
       //   "https://dog.ceo/api/breed/ovcharka/images/random/1"
       // );
       // const finalRes_photos = await res_photos.json();
       // this.listPhotos = finalRes_photos["message"];
       // this.pic = this.listPhotos;
+    },
+    async nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        await this.getData(this.currentPage);
+      }
+    },
+    async prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        await this.getData(this.currentPage);
+      }
     },
   },
   mounted() {
@@ -45,6 +66,19 @@ export default {
 </script>
 
 <style scoped>
+.pagination {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem;
+}
+.pages-span {
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 1rem;
+}
+
 .dogs-container {
   display: flex;
   flex-direction: row;
@@ -66,7 +100,6 @@ export default {
   border-radius: 0.5rem;
   justify-content: center;
   align-items: center;
-
 }
 
 /* On mouse-over, add a deeper shadow */
