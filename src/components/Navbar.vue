74cc6e5c2edb0a4f23561004cd218a789cf4bf35
alfre <alfredo.medina@uc.cl>
2023-06-05 22:53:05 -0400

    navbar fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -72,7 +72,7 @@ export default {
 }
 
 /* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
-@media screen and (max-width: 600px) {
+/* @media screen and (max-width: 600px) {
   .topnav a:not(:first-child) {
     display: none;
   }
@@ -80,10 +80,10 @@ export default {
     float: right;
     display: block;
   }
-}
+} */
 
 /* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
-@media screen and (max-width: 600px) {
+/* @media screen and (max-width: 600px) {
   .topnav.responsive {
     position: relative;
   }
@@ -97,5 +97,5 @@ export default {
     display: block;
     text-align: left;
   }
-}
+} */
 </style>
