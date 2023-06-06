68380715d4dcad7c07f1538e2880408414996e94
Rocio Torres <rocio.tp@uc.cl>
2023-06-06 13:18:00 -0400

    Ready all

--- src/components/DogImage.vue
+++ src/components/DogImage.vue
@@ -16,18 +16,29 @@ export default {
 
   methods: {
     async getData() {
-      if (!this.breeds.hasOwnProperty(this.data["name"]) || this.breeds[this.data["name"]] === "") {
+      if (this.breeds[this.data["name"]] === "none"){
         this.pic =
           "https://images.assetsdelivery.com/compings_v2/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016.jpg";
         return;
       }
-      const res_photos = await fetch(
+      if(this.breeds.hasOwnProperty(this.data["name"]) && this.breeds[this.data["name"]] === ""){
+        const name_api = this.data["name"].toLowerCase().replace(/ /g, "_").replace("'", "’");
+        if (name_api === "bavarian_mountain_scent_houn"){
+          this.pic = "https://api-ninjas.com/images/dogs/"+ name_api +"d.jpg";
+        }
+        else{
+          this.pic = "https://api-ninjas.com/images/dogs/"+ name_api +".jpg";
+        }
+        return;
+        }
+      else {
+        const res_photos = await fetch(
         `https://dog.ceo/api/breed/${
           this.breeds[this.data["name"]]
         }/images/random/1`
       );
       const finalRes_photos = await res_photos.json();
-      this.pic = finalRes_photos["message"];
+      this.pic = finalRes_photos["message"];}
     },
   },
   mounted() {
